c2e79ee2a57d54df59f2d3fea8ac035d5f991b7f
José Henrique <josehenrique.tropalde@gmail.com>
2022-12-13 13:17:25 -0300

    Revert "update ads"
    This reverts commit d5f478b8d41733a0e14670245224dd5f83949f89.

--- pages/characters.tsx
+++ pages/characters.tsx
@@ -128,6 +128,14 @@ const Characters = ({subscription}) => {
                 layout="in-article"
                 format="auto"
               />
+              <Adsense
+                className='adsbygoogle'
+                client="ca-pub-2203319803462882"
+                slot="6666420125"
+                style={{ display: 'block', textAlign: 'center', marginTop: 16}}
+                layout="in-article"
+                format="auto"
+              />
             </InArticleAds> 
             </>
           )
